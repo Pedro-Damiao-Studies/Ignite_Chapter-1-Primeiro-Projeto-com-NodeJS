5cf1ed68f8a80841f24ee3f1c00f6085e25c07cf
Pedro Henrique Ferreira <pedrohenriquefcd@gmail.com>
2022-05-29 17:17:18 -0300

    Remover conta

--- README.md
+++ README.md
@@ -18,7 +18,9 @@
 
 - [X] Deve ser possível obter dados da conta do cliente
 
-- [ ] Deve ser possível deletar uma conta
+- [X] Deve ser possível deletar uma conta
+
+- [X] Deve ser possível retornar o balanço da conta
 
 ---
 
@@ -32,6 +34,6 @@
 
 - [X] Não deve ser possível fazer saque em uma conta não existente
 
-- [ ] Não deve ser possível excluir uma conta não existente
+- [X] Não deve ser possível excluir uma conta não existente
 
 - [X] Não deve ser possível fazer saque quando o saldo for insuficiente
--- src/index.js
+++ src/index.js
@@ -125,4 +125,20 @@ app.get('/accounts', verifyIfAccountExistsByCPF, (request, response) => {
   return response.json(customer);
 });
 
+app.delete('/accounts', verifyIfAccountExistsByCPF, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
+app.get('/balance', verifyIfAccountExistsByCPF, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement)
+
+  return response.json(balance);
+});
+
 app.listen(3333);
